815f3047d88826a28aaae646b7c27c326a07698e
carolina <carolina@neocoast.com>
2019-04-29 11:11:54 -0300

    add dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
+  authenticated :user do
+    root to: 'dashboard#index'
+  end
+  
   root to: 'home#index'
+
   get 'faq', to: 'home#faq'
   get 'countries/cities/:country_id', to: 'countries#cities'
 end
