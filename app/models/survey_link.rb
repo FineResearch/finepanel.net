a225a11cc777d8c613e7b721248dfb4681e55460
carolina <carolina@neocoast.com>
2019-05-13 11:34:23 -0300

    feedback for list valid surveys and payment info

--- app/models/survey_link.rb
+++ app/models/survey_link.rb
@@ -4,4 +4,8 @@ class SurveyLink < ApplicationRecord
   def valid_link?
     ConfirmitGateway.valid_survey_link?(link) && created_at >= Time.now - 60.days
   end
+
+  def data_from_valid_survey
+    ConfirmitGateway.data_from_valid_survey(link)
+  end
 end
