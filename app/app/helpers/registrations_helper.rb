d5238e93e16a18e70db37abf17c6fa93eece802f
carolina <carolina@neocoast.com>
2019-07-11 14:51:13 -0300

    edit user profile

--- app/app/helpers/registrations_helper.rb
+++ app/app/helpers/registrations_helper.rb
@@ -8,28 +8,29 @@ module RegistrationsHelper
     options_for_select(countries.sort, country_id)
   end
 
-  def options_for_city_select(country_id = 1)
+  def options_for_city_select(country_id = 1, city_id = nil)
     cities = ConfigurationReader.cities(country_id).map { |city| [t(city[0]), city[1]] }.sort
     if cities.blank?
       options_for_select([])
     else
-      options_for_select(cities, nil)
+      options_for_select(cities, city_id)
     end
   end
 
-  def options_for_suffix_select(_resource = nil)
+  def options_for_suffix_select(_resource = nil, suffix = nil)
     suffixes = ConfigurationReader.suffixes
-    options_for_select(create_array_for_select(suffixes, 'suffixes.'))
+    suffix[:selected] = suffix[:selected].split('.')[0] if suffix[:selected].present?
+    options_for_select(create_array_for_select(suffixes, 'suffixes.'), suffix)
   end
 
-  def options_for_specialty_select(_resource = nil)
+  def options_for_specialty_select(_resource = nil, specialty_id = nil)
     specialties = ConfigurationReader.specialties
-    options_for_select(create_array_for_select(specialties, 'specialties.s'))
+    options_for_select(create_array_for_select(specialties, 'specialties.s'), specialty_id)
   end
 
-  def options_for_work_at_select(_resource = nil)
+  def options_for_work_at_select(_resource = nil, work_at_id = nil)
     work_at_options = ConfigurationReader.work_at_options
-    options_for_select(create_array_for_select(work_at_options, 'work_at_options.w'))
+    options_for_select(create_array_for_select(work_at_options, 'work_at_options.w'), work_at_id)
   end
 
   private
