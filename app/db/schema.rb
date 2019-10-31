c35db622f08fb14c8346e50f67429986a5ed18c9
Maximiliano Kotvinsky <maximiliano.kotvinsky@moove-it.com>
2019-10-31 16:11:48 -0300

    Add schema migrations for active and language

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_180334) do
+ActiveRecord::Schema.define(version: 2019_10_17_140308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 2019_07_24_180334) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remember_token"
+    t.boolean "active_app"
+    t.integer "language"
     t.index ["encrypted_email"], name: "index_users_on_encrypted_email", unique: true
     t.index ["spanel"], name: "index_users_on_spanel"
   end
