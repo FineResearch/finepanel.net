5ca1b8d98a201237537b5d352db6fbc4ccf7b519
Juanchi <juan.zeni@moove-it.com>
2020-02-17 12:46:44 -0300

    Fix migration file name

--- app/db/migrate/20200213161244_add_unique_to_survey.rb
+++ app/db/migrate/20200213161244_add_unique_to_survey.rb
@@ -1,4 +1,4 @@
-class AddUniqueConstraints < ActiveRecord::Migration[5.2]
+class AddUniqueToSurvey < ActiveRecord::Migration[5.2]
   def change
     add_index :survey_links, [:project_id, :resp_id, :spanel, :link, :variables], unique: true, name: :unique_survey_links
   end
