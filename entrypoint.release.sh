cb928f1d91c0a97b587933108f36f0c49fefc214
Juanchi <juan.zeni@moove-it.com>
2021-08-27 16:47:22 -0300

    Modify entrypoint

--- entrypoint.release.sh
+++ entrypoint.release.sh
@@ -5,6 +5,8 @@ if [ -f /app/tmp/pids/server.pid ]; then
   rm -f /app/tmp/pids/server.pid
 fi
 
+export $(echo $SECRETS | jq -j "to_entries|map(\"\(.key)=\(.value|tostring) \")|.[]")
+
 if [ $AUTOMATICALLY_MIGRATE ]; then
   bundle exec rake db:migrate
 fi
