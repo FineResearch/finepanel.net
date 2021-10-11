421d644b47e77813c9e82c6e8e886d4c21b62f5d
Juan David Gaviria Agudelo <45491405+MrJuanGaviriaK@users.noreply.github.com>
2021-10-11 11:44:56 -0500

    Add survey information to login flow (#125)

--- app/app/controllers/api/v1/sessions_controller.rb
+++ app/app/controllers/api/v1/sessions_controller.rb
@@ -10,7 +10,8 @@ module Api
         if user.present?
           sign_in(resource_name, user)
           @current_user = user
-          render json: { email: user.email, userInfo: {}}, status: :ok
+          survey_list = ConfirmitGateway.get_surveys_for_user(@current_user, @current_user.user_respid(@current_user.email))
+          render json: { email: user.email, userInfo: {surveys: survey_list}}, status: :ok
         else
           render json: {message: 'User not found'}, status: :not_found
         end
