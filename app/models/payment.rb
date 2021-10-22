f4a0e0fd6179bcc19cafbdf5cc33d2a903cddccf
Juan David Gaviria Agudelo <45491405+MrJuanGaviriaK@users.noreply.github.com>
2021-10-22 10:34:54 -0500

    FN-139 Adjustments to Creditos y Pagos page. (#133)
    * Crete new payment scope
    
    * Rename payment scope

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class Payment < ApplicationRecord
+
+  # -- Scopes --
+  scope :with_credit, -> { where.not(credit: 0) }
+
   def self.get_payment_history_for_user(respid)
-    Payment.where(respid: respid).order(email_date: :desc)
+    Payment.where(respid: respid).with_credit.order(email_date: :desc)
   end
 end
