2c0be40ed31b31face3753c489849ec25890fdb7
Juan David Gaviria Agudelo <45491405+MrJuanGaviriaK@users.noreply.github.com>
2021-12-02 07:37:40 -0500

    Fix date time issue with payment history

--- app/presenters/payment_summary_presenter.rb
+++ app/presenters/payment_summary_presenter.rb
@@ -3,7 +3,7 @@ PaymentSummaryPresenter = Struct.new(:payments, :currency, :history) do
     py = payments.present? ? payments[:total] : '0'
     ht = history.present? ? history.credit.to_s : ''
     cr = currency.try(:delete, '$')
-    date = history.present? ? history.email_date.strftime("%d-%m-%Y") : ''
+    date = history.present? ? history.email_date.strftime("%d-%m-%Y") : Time.now
     record = history.present?
 
     { payments: py, history: ht, currency: cr, date: date, record: record }
